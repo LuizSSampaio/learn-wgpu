3966dd521c9f8456f8c252390eb97d4d467c6b3b
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2025-05-13 17:31:57 -0300

    Hello window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,59 @@
-fn main() {
-    println!("Hello, world!");
+use winit::{
+    application::ApplicationHandler,
+    event::{ElementState, KeyEvent, WindowEvent},
+    event_loop::{ActiveEventLoop, ControlFlow, EventLoop},
+    keyboard::{KeyCode, PhysicalKey},
+    window::{self, Window, WindowAttributes},
+};
+
+#[derive(Default)]
+struct App {
+    window: Option<Window>,
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        let window = event_loop
+            .create_window(WindowAttributes::default())
+            .unwrap();
+        self.window = Some(window);
+    }
+
+    fn window_event(
+        &mut self,
+        event_loop: &ActiveEventLoop,
+        window_id: window::WindowId,
+        event: winit::event::WindowEvent,
+    ) {
+        if let Some(window) = &self.window {
+            if window_id == window.id() {
+                match event {
+                    WindowEvent::CloseRequested
+                    | WindowEvent::KeyboardInput {
+                        event:
+                            KeyEvent {
+                                state: ElementState::Pressed,
+                                physical_key: PhysicalKey::Code(KeyCode::Escape),
+                                ..
+                            },
+                        ..
+                    } => {
+                        event_loop.exit();
+                    }
+                    WindowEvent::RedrawRequested => {
+                        self.window.as_ref().unwrap().request_redraw();
+                    }
+                    _ => {}
+                }
+            }
+        }
+    }
+}
+
+fn main() -> Result<(), winit::error::EventLoopError> {
+    let event_loop = EventLoop::new().unwrap();
+    let mut app = App::default();
+
+    event_loop.set_control_flow(ControlFlow::Poll);
+    event_loop.run_app(&mut app)
 }
